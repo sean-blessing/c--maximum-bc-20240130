eb387f61ca1ae942fa646afabf2ec6748baff10a
Sean Blessing <snblessing@gmail.com>
2024-02-26 13:30:22 -0500

    LabArrayList and Exceptions

---
+++ day-8-interfaces/LabArrayList/LabArrayList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Diagnostics;
+
+namespace LabArrayList {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Lists App!");
+
+            string[] daysOfWeek = { "Sunday", "Monday", "Second Monday", "Hump Day",
+                                    "First Friday", "Second Friday", "Saturday"};
+
+            for (int i = 0; i < daysOfWeek.Length; i++) {
+                Console.WriteLine(daysOfWeek[i]);
+            }
+
+            DateTime today = DateTime.Now;
+            Console.WriteLine("Today (dow): "+today.DayOfWeek);
+            Console.WriteLine("Today (dow#): " + (int)today.DayOfWeek);
+
+            ArrayList dow = new ArrayList{"Sunday", "Monday", "Tuesday", "Wednesday",
+                                                  "Thursday", "Friday", "Saturday", "Sunday"};
+            foreach (string day in dow) { 
+                Console.WriteLine($"{day}");
+            }
+
+
+            Console.WriteLine("bye");
+        }
+    }
+}
---
+++ day-9/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Globalization;
+
+namespace Exceptions {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, Exceptions!");
+            Boolean success = false;
+
+            DateTime dateTime = DateTime.Now;
+
+            // Use Exception Handling to validate a date
+            while (!success) { 
+                Console.WriteLine("Let's enter a date.");
+                int monthNbr = getInt("Enter Month Number (1 - 12): ", 1, 12);
+                //Console.WriteLine($"Month: " + DateTimeFormatInfo.CurrentInfo.GetAbbreviatedMonthName(monthNbr));
+
+                int day = getInt("Enter Day Number (1 - 31): ", 1, 31);
+                //Console.WriteLine($"Day of Week: " + Enum.GetName(typeof(DayOfWeek), (dowNbr-1)));
+
+                int year = getInt("Enter Year: ");
+
+                try {
+                    dateTime = new DateTime(year, monthNbr, day);
+                    Console.WriteLine($"Date is: {dateTime.ToShortDateString()}");
+                    success = true;
+                }
+                catch (System.ArgumentOutOfRangeException saoore) {
+                    Console.WriteLine(saoore.Message);
+                    Console.WriteLine($"Year ({year}) / Month ({monthNbr}) / Day ({day})");
+                    Console.WriteLine("Try again.");
+                    success = false;
+                }
+            }
+            Console.WriteLine("bye");
+        }
+
+        // Use data validation to validate a whole number within a range
+        private static int getInt(string prompt, int min, int max) {
+            int monthNbr = 0;
+            Boolean success = false;
+
+            while (!success) {
+                monthNbr = getInt(prompt);
+                if (monthNbr < min) {
+                    Console.WriteLine($"Input must be greater than, or equal to, {min}.");
+                    Console.WriteLine("Please try again.\n");
+                    success = false;
+                }
+                else if (monthNbr > max) {
+                    Console.WriteLine($"Input must be less than, or equal to, {max}.");
+                    Console.WriteLine("Please try again.\n");
+                    success = false;
+                }
+                else { 
+                    success = true;
+                }
+            }
+
+            return monthNbr;
+        }
+
+        // Use exception handling to get a valid whole number
+        private static int getInt(string prompt) {
+            int nbr = 0;
+            Boolean success = false;
+
+            while (!success) {
+                try {
+                    Console.Write(prompt);
+                    nbr = Int32.Parse(Console.ReadLine());
+                    success = true;
+                }
+                catch (System.FormatException sfe) {
+                    Console.WriteLine(sfe.Message);
+                    Console.WriteLine("Please enter a whole number.\n");
+                    success = false;
+                }
+            }
+            return nbr;
+        }
+    }
+}
