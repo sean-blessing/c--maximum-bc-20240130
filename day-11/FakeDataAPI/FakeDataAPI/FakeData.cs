fa9a9b4fa78cebce4b93fa15bef4df020e7f2af2
Sean Blessing <snblessing@gmail.com>
2024-02-29 16:36:50 -0500

    web api first day

---
+++ day-11/FakeDataAPI/FakeDataAPI/FakeData.cs
@@ -0,0 +1,11 @@
+﻿namespace FakeDataAPI {
+    
+public interface IStringListService {
+        List<string> StringList { get; set; }
+    }
+
+    public class StringCollection : IStringListService {
+        List<string> _stringList = new List<string> { "red", "blue" };
+        public List<string> StringList { get { return _stringList; } set { _stringList = value; } }
+    }
+}
